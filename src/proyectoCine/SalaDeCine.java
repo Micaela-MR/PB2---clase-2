4360aabfb3657718ad3a3728326b05780a4bb57b
Micaela Mendez <micaela.mr03@gmail.com>
2023-04-09 21:45:06 -0300

    Initial commit

---
+++ src/proyectoCine/SalaDeCine.java
@@ -0,0 +1,12 @@
+package proyectoCine;
+
+public class SalaDeCine {
+	
+	private Integer butacasTotales;
+	private Integer cantidadDeFilas;
+	
+	public SalaDeCine(Integer butacasTotales, Integer cantidadDeFilas){
+		this.butacasTotales = butacasTotales;
+		this.cantidadDeFilas = cantidadDeFilas;
+	}
+}
